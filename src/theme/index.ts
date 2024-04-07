6b80dbf35bb8e8a3f96826806bc9291f94cdaaa0
rkaasi <rkaasi@ttu.ee>
2024-04-07 17:53:48 +0300

    add theming

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+  }
+`;
+
+export const theme = {
+	lightBlue: "#EDF2F7",
+	white: "#FFFFFF",
+};
