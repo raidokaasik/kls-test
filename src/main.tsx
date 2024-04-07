184ef394e5eeea89688090102d7a8eddf641bc4f
rkaasi <rkaasi@ttu.ee>
2024-04-07 17:54:29 +0300

    add global style

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
-import "./global.css";
+import { GlobalStyle } from "./theme/index.ts";
 
 async function initServiceWorker() {
 	const { serviceWorker } = await import("./mock/serviceWorker.ts");
@@ -14,6 +14,7 @@ async function bootstrapApp() {
 	await initServiceWorker();
 	ReactDOM.createRoot(document.getElementById("root")!).render(
 		<React.StrictMode>
+			<GlobalStyle />
 			<Provider store={store}>
 				<App />
 			</Provider>
